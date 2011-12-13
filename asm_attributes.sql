efd1d3ddf23d6d8b6edc85a2fafad82c7c119cd5
Andy Colvin <acolvin@gmail.com>
2011-12-12 20:43:38 -0600

    added file

---
+++ asm_attributes.sql
@@ -0,0 +1,18 @@
+set pages 1000
+set lines 150
+col "Diskgroup" for a10
+col "Attribute" for a30
+col "Value" for a20
+select
+  g.name "Diskgroup",
+  a.name "Attribute",
+  a.value "Value"
+from
+  v$asm_diskgroup g,
+  v$asm_attribute a
+where
+  a.group_number=g.group_number
+and
+  a.name not like 'template%'
+order by 1,2
+/
